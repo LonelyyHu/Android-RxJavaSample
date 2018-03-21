4b37e390b725bf6efeeb4634d82fbd8ea2d97167
lonelyyhu <lonelyyhu@noodoe.com>
2018-03-21 15:49:43 +0800

    add blockForEach sample

--- app/src/main/java/com/lonelyyhu/exercise/rxjavasample/Test.java
+++ app/src/main/java/com/lonelyyhu/exercise/rxjavasample/Test.java
@@ -2,6 +2,10 @@ package com.lonelyyhu.exercise.rxjavasample;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -10,6 +14,7 @@ import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
@@ -23,6 +28,71 @@ public class Test {
 
     public static void main(String[] args) {
 
+        String[] ary = {"1", "2", "3", "4"};
+
+        Observable<Long> values = Observable.interval(100, TimeUnit.MILLISECONDS);
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        Observable.fromIterable(list)
+
+//        values.take(5)
+//        Observable.fromArray(ary)
+
+                .subscribeOn(Schedulers.io())
+
+
+                .flatMap(new Function<String, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(String s) throws Exception {
+                        System.out.println(s);
+                        return Observable.just("A-"+s);
+                    }
+                })
+
+
+                .blockingForEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        System.out.println(s);
+                    }
+                });
+
+
+//                .subscribe(new Observer<String>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//                        System.out.println("onSubscribe");
+//                    }
+//
+//                    @Override
+//                    public void onNext(String s) {
+//                        System.out.println("onNext:"+s);
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        System.out.println("onError");
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        System.out.println("onComplete");
+//
+//                    }
+//                });
+
+        System.out.println("finish");
+
+    }
+
+    public static void retryTest() {
         Observable.just("Hellow")
                 .flatMap(new Function<String, ObservableSource<String>>() {
                     @Override
@@ -121,8 +191,7 @@ public class Test {
                     public void onComplete() {
 
                     }
-        });
-
+                });
     }
 
     public static Observable<String> getObserver() {
